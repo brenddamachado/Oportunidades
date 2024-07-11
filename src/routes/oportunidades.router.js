4fba730ad4292acc45cea5b3deb56f80d952d55d
brenddamachado <brenda16machado.bm@gmail.com>
2024-07-10 23:52:30 -0300

    feat(delete): adicionar router delete

--- src/routes/oportunidades.router.js
+++ src/routes/oportunidades.router.js
@@ -1,20 +1,33 @@
 import { Router } from "express";
 import { inserindoVagas, retotnarVagas, buscarApoio, buscarPorTipo, excluirVaga } from "../controllers/oportunidades.controller";
 
-const vagaRouter = Router()
+const router = express.Router()
 
-vagaRouter.post("/vagas", (req, res) =>{
+router.post("/vagas", (req, res) =>{
     const {tipo, titulo, descricao, link} = req.body;
     const novaVaga = inserindoVagas(tipo,titulo,descricao,link);
     res.json({novaVaga})
 })
 
-Router.length("/vagas", (req, res)=>{
+router.length("/vagas", (req, res)=>{
     const listaVaga = retotnarVagas()
     res.json({listaVaga})
 })
 
-Router.get('/vagas/tipo/:tipo', (req, res)=>{
+router.get('/vagas/apoio', (req, res) => {
+    res.json(buscarApoio());
+  });
+
+router.get('/vagas/tipo/:tipo', (req, res)=>{
     const tipoVaga = req.params.tipo;
     res.json(buscarPorTipo(tipoVaga))
 })
+
+router.delete('/vagas/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const vagaExcluida = excluirVaga(id);
+    if (!vagaExcluida) {
+      return res.status(404).send('Vaga não encontrada.');
+    }
+    res.json(vagaExcluida);
+  });
